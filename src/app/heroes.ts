b0e4a0d0f925139f2bc127cb27d09bbb80a6278b
Glenn Dasai <glenn.dasai@gmail.com>
2018-04-23 20:17:07 +0200

    Added prefix and fixed firstname

--- src/app/heroes.ts
+++ src/app/heroes.ts
@@ -1,7 +1,8 @@
 import {NgbDate} from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-date';
 
 export class Hero {
-  name: string;
+  firstname: string;
+  prefix: string;
   surname: string;
   dateofbirth: NgbDate;
   profession: string;
